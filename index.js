463c702dd9ceab6046f27a45183c0c0fe9d85613
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-28 02:04:30 +0530

    Updated Routes

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app=express();
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use('view engine','ejs');
-app.use(bodyParser.urlencoded());
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req,res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', (req,res) => {
+    res.render('login.ejs');
+});
+
+app.get('/signup', (req,res) => {
+    res.render('signup.ejs');
+});
+
+app.listen(port, () => {
+    console.log("Server up at " + port);
+})
