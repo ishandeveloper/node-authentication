7e931cca1d5dc2012660d64eefd9191fa66ef7e1
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-28 02:44:30 +0530

    Added Level 1

---
+++ Level 1/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/public', express.static('public'));
+////////////////////////////////////ROUTES///////////////////////////////
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup.ejs');
+});
+
+///////////////////////CONNECTING TO DATABASE/////////////////////////////
+
+mongoose.connect("mongodb://localhost:27017/authentication", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("CONNECTED TO MONGODB!");
+});
+
+
+///////////////////////USERS/////////////////////
+
+const userSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const User = mongoose.model('user', userSchema);
+
+app.post('/signup', (req, res) => {
+    const newUser = User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((e) => {
+        if (!e) {
+            res.render('secrets.ejs');
+        } else {
+            console.log(e);
+        }
+    });
+
+});
+
+///////////////////////////////AUTHENTICATING USERS////////////////////////////////////////
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+    const user = req.body.username;
+    const pass = req.body.password;
+
+    User.findOne({
+        username: user
+    }, (e, matchedUser) => {
+        if (matchedUser.password == pass) {
+            res.render('secrets.ejs');
+        } else {
+            console.log("ERROR");
+        }
+    });
+
+});
+
+app.listen(port, () => {
+    console.log("Server up at " + port);
+})
